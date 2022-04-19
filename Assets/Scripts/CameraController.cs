b75f9a60bf4ec7ffc1d2dbc04fcdf0d64c5ec544
Max Schacherbauer <maximilian.schacherbauer@faktorzehn.de>
2022-04-19 07:53:16 +0200

    E07 :: Add camera controls

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-        // Update is called once per frame
-    void Update()
+    public float panSpeed = 30f;
+    public float scrollSpeed = 150f;
+    public float minCameraHeight = 10;
+    public float maxCameraHeight = 180;
+    public float panBoardThickness = 10f;
+    private bool _isCameraMovementActive = true;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) _isCameraMovementActive = !_isCameraMovementActive;
+        if (!_isCameraMovementActive) return;
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBoardThickness)
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBoardThickness)
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBoardThickness)
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBoardThickness)
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        var scrollDelta = Input.mouseScrollDelta.y * scrollSpeed * Time.deltaTime;
+        var updatedCameraPosition = transform.position;
+        updatedCameraPosition.y = Mathf.Clamp(updatedCameraPosition.y - scrollDelta, minCameraHeight, maxCameraHeight);
+        transform.position = updatedCameraPosition;
     }
 }
